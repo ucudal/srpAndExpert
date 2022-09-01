d78d75d7bc2bf048c1e155b7bb46ae3b17c2fb03
EmiLabarthe <emiliano.labarthe@correo.ucu.edu.uy>
2022-09-01 16:43:34 -0300

    Ejercicio 2 hecho.

--- Ejercicio 2/src/Library/AppointmentService.cs
+++ Ejercicio 2/src/Library/AppointmentService.cs
@@ -3,9 +3,10 @@ using System.Text;
 
 namespace Library
 {
-    public class AppointmentService
-    {
-        public static string CreateAppointment(string name, string id, string phoneNumber, DateTime date, string appoinmentPlace, string doctorName)
+    public class AppointmentService // Se deberían crear otras clases para guardar los datos de los objetos interactuando en una consulta y que cada clase se ocupe solo de un aspecto
+    { 
+    //También se agregan otros datos útiles, como la edad del paciente, el nro de consulta, la especialidad del doctor.
+        public static string CreateAppointment(string name, string id, string phoneNumber, DateTime date, string appoinmentPlace, string doctorName, string appointmentCode, string edadPaciente, string doctorSpeciality )
         {
             StringBuilder stringBuilder = new StringBuilder("Scheduling appointment...\n");
             Boolean isValid = true;
@@ -41,13 +42,73 @@ namespace Library
                 isValid = false;
             }
 
+            if (string.IsNullOrEmpty(appointmentCode))
+            {
+                stringBuilder.Append("Unable to schedule appointment, 'appointment code' is required\n");
+                isValid = false;
+            }
+            
+            if (string.IsNullOrEmpty(edadPaciente))
+            {
+                stringBuilder.Append("Unable to schedule appointment, 'edad paciente' is required\n");
+                isValid = false;
+            }
+            
+            if (string.IsNullOrEmpty(doctorSpeciality))
+            {
+                stringBuilder.Append("Unable to schedule appointment, 'doctor Speciality' is required\n");
+                isValid = false;
+            }
+
             if (isValid)
             {
                 stringBuilder.Append("Appoinment scheduled");
+                new Patient (name, id, phoneNumber, edadPaciente);
+                new Doctor (doctorName, doctorSpeciality);
+                new Appointment (date,appoinmentPlace,appointmentCode);
+
             }
 
             return stringBuilder.ToString();
         }
 
     }
+    public class Patient //Clase para guardar los datos del paciente
+    {
+        public string name { get; set; }
+        public string id { get; set; }
+        public string phoneNumber { get; set; }
+        public string edadPaciente { get; set; }
+        
+        public Patient (string name, string id, string phoneNumber, string edadPaciente)
+        {
+            this.name = name;
+            this.id = id;
+            this.phoneNumber = phoneNumber;
+            this.edadPaciente = edadPaciente;        
+        }
+    }
+    public class Doctor //Clase para guardar los datos del doctor
+    {
+        public string doctorName { get; set;}
+        public string doctorSpeciality { get; set; }
+        public Doctor(string doctorName, string doctorSpeciality)
+        {
+            this.doctorName = doctorName;
+            this.doctorSpeciality = doctorSpeciality;
+        }
+    }
+    public class Appointment // Clase para guardar los datos del appointment
+    {
+        public DateTime date;
+        public string appointmentPlace;
+        public string appointmentCode;
+        public Appointment(DateTime date, string appoinmentPlace, string appointmentCode)
+        {
+            this.date = date;
+            this.appointmentPlace = appoinmentPlace;
+            this.appointmentCode = appointmentCode;
+        }
+    }
+    
 }
--- Ejercicio 2/src/Program/Program.cs
+++ Ejercicio 2/src/Program/Program.cs
@@ -7,10 +7,10 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            string appointmentResult = AppointmentService.CreateAppointment("Steven Jhonson", "986782342", "5555-555-555", DateTime.Now, "Wall Street", "Armand");
+            string appointmentResult = AppointmentService.CreateAppointment("Steven Jhonson", "986782342", "5555-555-555", DateTime.Now, "Wall Street", "Armand","3245","45","Oculista");
             Console.WriteLine(appointmentResult);
 
-            string appointmentResult2 = AppointmentService.CreateAppointment("Ralf Manson", "", "5555-555-555", DateTime.Now, "Queen Street", "");
+            string appointmentResult2 = AppointmentService.CreateAppointment("Ralf Manson", "457890658", "5555-555-555", DateTime.Now, "Queen Street", "Maradona","3345","24","Otorrino");
             Console.WriteLine(appointmentResult2);
         }
     }
