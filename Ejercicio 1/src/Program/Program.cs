671ee9b2095bd2a383a8c13217826a34ce4adfc8
EmiLabarthe <emiliano.labarthe@correo.ucu.edu.uy>
2022-09-05 08:30:32 -0300

    commit finalizado

--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -7,8 +7,8 @@ namespace SRP
     {
         static void Main()
         {
-            Book book1 = new Book("Design Patterns","Erich Gamma & Others","001-034");
-            Book book2 = new Book("Pro C#","Troelsen","001-035");
+            Book book1 = new Book("Design Patterns","Erich Gamma & Others","1");
+            Book book2 = new Book("Pro C#","Troelsen","2");
             BookPlace book1Place = new BookPlace("A","7");
             BookPlace book2Place = new BookPlace("B","3");
             Console.WriteLine($"El libro {book1.Title} está en {book1Place.LibrarySector},{book1Place.LibraryShelve}");
